bf65eea7d1ed779fd8e464f1db5afd7fd72c1f41
Leonardo <leorsantos2003@gmail.com>
2025-08-01 23:46:42 -0300

    entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    # views.index -> index é uma função em views.py
+    # name="index" -> index é um nome arbitrário para a rota (url)
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry_page, name="entry")
 ]
+
+# wiki/<str:title> cria um padrão de URL.
+# Qualquer coisa depois de wiki/ será capturada como uma string e passada para a view com o nome de title.
+# views.entry_page é o nome da função de view que vamos criar no próximo passo para lidar com essa rota.
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -8,3 +9,20 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# Função que receberá o title da url,
+# busca o arquivo corresponde com util.get_entry()
+# e renderiza uma nova página com o conteúdo.
+def entry_page(request, title):
+    content_md = util.get_entry(title)
+    if content_md is None:
+        # Se a entrada não existir, mostre uma página de erro (vamos criá-la)
+        return render(request, "encyclopedia/error.html", {
+            "message": f"A página '{title}' não foi encontrada."
+        })
+    else:
+        content_html = markdown2.markdown(content_md)
+        # Se a entrada existir, mostre a página com o conteúdo
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": content_html
+        })
