8036cbd82c0b923d06e9500130cf260fa5039900
Leonardo <leorsantos2003@gmail.com>
2025-08-02 16:19:10 -0300

    search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     # views.index -> index é uma função em views.py
     # name="index" -> index é um nome arbitrário para a rota (url)
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry_page, name="entry")
+    # entry recebida é envaida para a função entry_page em views
+    path("wiki/<str:title>", views.entry_page, name="entry"),
+    path("search", views.search, name="search")
 ]
 
 # wiki/<str:title> cria um padrão de URL.
 # Qualquer coisa depois de wiki/ será capturada como uma string e passada para a view com o nome de title.
-# views.entry_page é o nome da função de view que vamos criar no próximo passo para lidar com essa rota.
+# views.entry_page é o nome da função de view para lidar com essa rota.
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 import markdown2
 
 from . import util
@@ -13,6 +15,7 @@ def index(request):
 # busca o arquivo corresponde com util.get_entry()
 # e renderiza uma nova página com o conteúdo.
 def entry_page(request, title):
+    # Procura um arquivo md com o título recebido e armazena o conteúdo do arquivo em content_md
     content_md = util.get_entry(title)
     if content_md is None:
         # Se a entrada não existir, mostre uma página de erro (vamos criá-la)
@@ -20,9 +23,33 @@ def entry_page(request, title):
             "message": f"A página '{title}' não foi encontrada."
         })
     else:
+        # Converte o texto de Markdown para HTML
         content_html = markdown2.markdown(content_md)
         # Se a entrada existir, mostre a página com o conteúdo
+        # Renderiza entry.html, enviando o título e o content já convertido
         return render(request, "encyclopedia/entry.html", {
             "title": title,
             "content": content_html
         })
+    
+# Função de busca
+def search(request):
+    # Pega o termo da busca da URL (?q=...)
+    query = request.GET.get('q', '')
+
+    # Pega a lista de todas as entradas
+    all_entries = util.list_entries()
+
+    # 1. Checa por um correspondência exata (ignora maiúsculas/minúsculas)
+    for entry in all_entries:
+        if query.lower() == entry.lower():
+            return HttpResponseRedirect(reverse("entry", args=[entry]))
+        
+    # 2. Se não houver correspondência exata, busca por substrings
+    results = [entry for entry in all_entries if query.lower() in entry.lower()]
+
+    # Renderiza a página de resultados, passando os resultados encontrados
+    return render(request, "encyclopedia/search_results.html", {
+        "results": results,
+        "query": query
+    })
